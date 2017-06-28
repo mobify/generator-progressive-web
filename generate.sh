0784cf5cadc3d018695e99d73db9b8b0f6b848c2
Jeff Kamo <kamojeffrey@gmail.com>
2017-06-27 17:40:20 -0700

    Add 'g' flag to end of sed replace command

--- generate.sh
+++ generate.sh
@@ -58,7 +58,7 @@ sed -i -e "s/Merlin's Potions/$project_name/g" web/app/static/manifest.json
 # To replace short_name in the manifest.json
 sed -i -e "s/Merlin's/$project_name/g" web/app/static/manifest.json
 # To replace variable values in _variable.scss with their respective ui_kit_value
-sed -i -e "s/\:.*ui_kit_value//" web/app/styles/_variables.scss
+sed -i -e "s/\:.*ui_kit_value//g" web/app/styles/_variables.scss
 
 # This is about the web
 cd web || exit
